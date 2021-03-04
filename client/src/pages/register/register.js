17381e77500ec297a0694f29f887f272a23dc367
Volosyuk Daniil <danya.volosyuk@gmail.com>
2021-03-04 23:11:49 +0300

    Register form completed

--- client/src/pages/register/register.js
+++ client/src/pages/register/register.js
@@ -1,65 +1,90 @@
 import React, { Component, Fragment } from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+
 import axios from 'axios';
-import ReactDOM from 'react-dom'
 
 class Register extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            login:'',
-            firstName:'',
-            lastName:'',
-            password :'',
-            repeatPassword:'',
+            passRegex: /[A-Za-z0-9!@#$%^&*]{4,20}/,
+            loginRegex: /[A-Za-z0-9]{4,20}/,
+            nameRegex: /^([A-Za-z]+|[А-Яа-я]+)$/,
+
+            globalErrorTitle: '',
+            globalError: '',
+
+            login: '',
+            firstName: '',
+            lastName: '',
+            password : '',
+            passwordConfirm: '',
+
+            loginError: '',
+            firstNameError: '',
+            lastNameError: '',
+            passwordError: '',
+            passwordConfirmError: '',
+
+            loading: false
         }
-        this.handleLoginOnChange = this.handleLoginOnChange.bind(this);
-        this.handleFirstNameOnChange = this.handleFirstNameOnChange.bind(this);
-        this.handleLastNameOnChange = this.handleLastNameOnChange.bind(this);
-        this.handlePasswordOnChange = this.handlePasswordOnChange.bind(this);
-        this.handleRepeatPasswordOnChange = this.handleRepeatPasswordOnChange.bind(this);
+
+        this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.hideGlobalError = this.hideGlobalError.bind(this);
     }
 
     render() {
         return (
             <Fragment>
-                <form onSubmit = {this.handleSubmit} className="ui form attached segment padded container text">
+                <form onSubmit = {this.handleSubmit} className="ui form attached segment padded container text" autoComplete="off">
                     <h2 className="ui header">Регистрация</h2>
-                    <div id="errorBlock"></div>
-                    <div className="field">
-                        <label>Логин</label>
-                        <input type="text" name="login" placeholder="Введите логин" value = {this.state.login} onChange = {this.handleLoginOnChange} />
+
+                    <div className={`ui negative message ${this.state.globalError ? '' : 'hidden'}`}>
+                        <i className="close icon" onClick={this.hideGlobalError}></i>
+                        <div className="header">{this.state.globalErrorTitle}</div>
+                        <p>{this.state.globalError}</p>
                     </div>
 
-
-                    <div className="field">
-                        <label>Имя</label>
-                        <div className="two fields">
-                            <div className="field">
-                                <input type="text" name="firtName" placeholder="Введите имя" value = {this.state.firstName} onChange = {this.handleFirstNameOnChange} />
-                            </div>
-                            <div className="field">
-                                <input type="text" name="lastName" placeholder="Введите фамилию" value = {this.state.lastName} onChange = {this.handleLastNameOnChange} />
-                            </div>
+                    <div className={`field required ${this.state.loginError ? 'error' : ''}`}>
+                        <label>Логин</label>
+                        <input type="text" name="login" placeholder="Введите логин" value = {this.state.login} onChange = {this.handleInputChange} />
+                        <div className={`ui basic red pointing prompt label ${this.state.loginError === 'empty' ? 'visible' : 'hidden'}`}>Введите логин</div>
+                        <div className={`ui basic red pointing prompt label ${this.state.loginError === 'regExp' ? 'visible' : 'hidden'}`}>Неверный формат логина</div>
+                    </div>
+    
+                    <div className="two fields">
+                        <div className={`field required ${this.state.firstNameError ? 'error' : ''}`}>
+                            <label>Имя</label>
+                            <input type="text" name="firstName" placeholder="Введите имя" value = {this.state.firstName} onChange = {this.handleInputChange} />
+                            <div className={`ui basic red pointing prompt label ${this.state.firstNameError === 'empty' ? 'visible' : 'hidden'}`}>Введите имя</div>
+                            <div className={`ui basic red pointing prompt label ${this.state.firstNameError === 'regExp' ? 'visible' : 'hidden'}`}>Неверный формат имени</div>
+                        </div>
+                        <div className={`field ${this.state.lastNameError ? 'error' : ''}`}>
+                            <label>Фамилия</label>
+                            <input type="text" name="lastName" placeholder="Введите фамилию" value = {this.state.lastName} onChange = {this.handleInputChange} />
+                            <div className={`ui basic red pointing prompt label ${this.state.lastNameError === 'regExp' ? 'visible' : 'hidden'}`}>Неверный формат фамилии</div>
                         </div>
                     </div>
-
-
-                    <div className="field">
+                    
+                    <div className="field required">
                         <label>Пароль</label>
                         <div className="two fields">
-                            <div className="field">
-                                <input type="password" name="password" placeholder="Введите пароль" value = {this.state.password} onChange = {this.handlePasswordOnChange} />
+                            <div className={`field ${this.state.passwordError ? 'error' : ''}`}>
+                                <input type="password" name="password" placeholder="Введите пароль" value = {this.state.password} onChange = {this.handleInputChange} />
+                                <div className={`ui basic red pointing prompt label ${this.state.passwordError === 'empty' ? 'visible' : 'hidden'}`}>Введите пароль</div>
+                                <div className={`ui basic red pointing prompt label ${this.state.passwordError === 'regExp' ? 'visible' : 'hidden'}`}>Неверный формат пароля</div>
                             </div>
-                            <div className="field">
-                                <input type="password" name="passwordConfirm" placeholder="Повторите пароль" value = {this.state.repeatPassword} onChange = {this.handleRepeatPasswordOnChange}  />
+                            <div className={`field ${this.state.passwordConfirmError ? 'error' : ''}`}>
+                                <input type="password" name="passwordConfirm" placeholder="Повторите пароль" value = {this.state.passwordConfirm} onChange = {this.handleInputChange}  />
+                                <div className={`ui basic red pointing prompt label ${this.state.passwordConfirmError === 'empty' ? 'visible' : 'hidden'}`}>Введите пароль повторно</div>
+                                <div className={`ui basic red pointing prompt label ${this.state.passwordConfirmError === 'notEqual' ? 'visible' : 'hidden'}`}>Пароли не совпадают</div>
                             </div>
                         </div>
                     </div>
                     
-                    <button className="ui blue button" type="submit">Создать аккаунт</button>
+                    <button className={`ui blue button ${this.state.loading ? 'loading' : ''}`} type="submit" disabled={this.state.loading ? 'disabled' : ''}>Создать аккаунт</button>
                 </form>
                 <div className="ui bottom attached warning message container text">
                     <i className="icon help"></i>
@@ -69,132 +94,135 @@ class Register extends Component {
         );
     }
 
-    handleSubmit(event){
+    hideGlobalError() {
+        this.setState({
+            globalErrorTitle: '',
+            globalError: ''
+        });
+    }
 
-        const pass_Regexp = /[A-Za-z0-9!@#$%^&*]{4,20}/;
-        const login_Regexp = /[A-Za-z0-9]{4,20}/;
-        const name_Regexp = /[A-Za-z0-9А-Яа-я]{4,20}/;
+    handleInputChange(e) {
+        this.setState(
+            {[e.target.name]: e.target.value}, 
+            () => this.validateField(e.target.name, e.target.value)
+        );
+    }
+
+    handleSubmit(event) {
+        console.log('press');
+        event.preventDefault();
+
+        let isValid = true;
+
+        if ( !this.validateField('login', this.state.login) ) isValid = false;
+        if ( !this.validateField('firstName', this.state.firstName) ) isValid = false;
+        if ( !this.validateField('lastName', this.state.lastName) ) isValid = false;
+        if ( !this.validateField('password', this.state.password) ) isValid = false;
+        if ( !this.validateField('passwordConfirm', this.state.passwordConfirm) ) isValid = false;
+
+        // Выход из функции, если хоть одно поле невалидно
+        if ( !isValid ) return;
         
 
-        event.preventDefault();
-        if(this.state.password === this.state.repeatPassword)
-        {
-            if(!login_Regexp.test(this.state.login))
-            {
-                ResponseError({
-                    label: "Ошибка",
-                    responseMessage: "Неверный формат логина!"
-                });
-            } 
+        this.setState({loading: true});        
+
+        let data = {
+            login : this.state.login,
+            firstName : this.state.firstName,
+            lastName : this.state.lastName,
+            password : this.state.password 
+        }
+
+        axios.post('http://localhost:3080/register', data)
+        .then(response => {
+            console.log(response);
+
+            if (response.data.ok) 
+                localStorage.setItem('token', response.data.token);
             else
-            {
-                if(!pass_Regexp.test(this.state.password))
-                {
-                    ResponseError({
-                        label: "Ошибка",
-                        responseMessage: "Неверный формат пароля!"
-                    });
-                }
-                else
-                {
-                    if(!name_Regexp.test(this.state.firstName))
-                    {
-                        ResponseError({
-                            label: "Ошибка",
-                            responseMessage: "Неверный формат имени!"
-                        });  
-                    }
-                    else
-                    {
-                        if(!name_Regexp.test(this.state.lastName))
-                        {
-                            ResponseError({
-                                label: "Ошибка",
-                                responseMessage: "Неверный формат фамилии!"
-                            });   
-                        }
-                        else
-                        {
-                            console.log("Данные введены корректно!")
-                            let data = {
-                                login : this.state.login,
-                                firstName : this.state.firstName,
-                                lastName : this.state.lastName,
-                                password : this.state.password 
-                                }
-                            console.log("Form submitted.");
-                
-                            axios.post('http://localhost:3080/register', data)
-                                .then(response => {
-                                console.log(response);
-                
-                                if (response.data.ok) {
-                                // Все гуд, запрос прошел
-                                // Сохраняем токен в localStorage
-                                localStorage.setItem('token', response.data.token);
-                
-                                // Получаем доступ к приватной страничке с использованием этого токена
-                                let token = localStorage.getItem('token');
-                                axios.get('http://localhost:3080/secret', {headers: {'Authorization': token}}).then(response => {
-                                    console.log(response);
-                                })
-                                } 
-                                else {
-                                // Ошибка регистрации. Выводим пользователю текст ошибки response.data.message
-                                ResponseError({
-                                    label: "Ошибка",
-                                    responseMessage: response.data.message
-                                });
-                                }
-                            }
-                            ); 
-                        }
-                    }
-                }
-            }
-        }
-        else {
-            ResponseError({
-                label: "Ошибка",
-                responseMessage: "Пароли не совпадают!"
+                this.setState({
+                    globalErrorTitle: 'Ошибка',
+                    globalError: response.data.message,
+                    loading: false
+                });
+        })
+        .catch(err => {
+            this.setState({
+                globalErrorTitle: 'Ошибка',
+                globalError: 'Ошибка соединения с сервером',
+                loading: false
             });
+        }); 
+       
+    }
+
+    validateField(fieldName, value) {
+        switch (fieldName) {
+            case 'login':
+                if (!value) {
+                    this.setState({loginError: 'empty'});
+                    return false;
+                } 
+                else if (!this.state.loginRegex.test(value)) {
+                    this.setState({loginError: 'regExp'});
+                    return false;
+                } 
+                else {
+                    this.setState({loginError: null});
+                    return true;
+                }
+            case 'firstName':
+                if (!value) {
+                    this.setState({firstNameError: 'empty'});
+                    return false;
+                } 
+                else if (!this.state.nameRegex.test(value)) {
+                    this.setState({firstNameError: 'regExp'});
+                    return false;
+                } 
+                else {
+                    this.setState({firstNameError: null});
+                    return true;
+                }
+            case 'lastName':
+                if (value && !this.state.nameRegex.test(value)) {
+                    this.setState({lastNameError: 'regExp'});
+                    return false;
+                }
+                else {
+                    this.setState({lastNameError: null});
+                    return true;
+                }
+            case 'password':
+                if (!value) {
+                    this.setState({passwordError: 'empty'});
+                    return false;
+                } 
+                else if (!this.state.passRegex.test(value)) {
+                    this.setState({passwordError: 'regExp'});
+                    return false;
+                }
+                else {
+                    this.setState({passwordError: null});
+                    return true;
+                }
+            case 'passwordConfirm':
+                if (!value) {
+                    this.setState({passwordConfirmError: 'empty'});
+                    return false;
+                } 
+                else if (this.state.password !== value) {
+                    this.setState({passwordConfirmError: 'notEqual'});
+                    return false;
+                }
+                else {
+                    this.setState({passwordConfirmError: null});
+                    return true;
+                }
         }
     }
 
-
-    handleLoginOnChange(event) {
-        console.log("Login Change");
-        this.setState({login : event.target.value})
-    }
-
-    handleFirstNameOnChange(event) {
-        this.setState({firstName : event.target.value})
-    }
-
-    handleLastNameOnChange(event) {
-        this.setState({lastName : event.target.value})
-    }
-
-    handlePasswordOnChange(event) {
-        console.log("Password Change");
-        this.setState({password : event.target.value})
-    }
-    handleRepeatPasswordOnChange(event) {
-        console.log("Repeat Password Change");
-        this.setState({repeatPassword : event.target.value})
-    }
-
-}
-
-function ResponseError({ label, responseMessage }) {
-    const element = (
-        <div className="ui negative message">
-            {/*<i className="close icon"></i>*/}
-            <div className="header">{label}</div>
-            <p>{responseMessage}</p>
-        </div>
-    );
-    ReactDOM.render(element, document.getElementById("errorBlock"));
+    
 }
 
 export default Register;
