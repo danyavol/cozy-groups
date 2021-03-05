ce4ba3dc709c32debe8cf42fdd8053bbd4b34ab9
Volosyuk Daniil <danya.volosyuk@gmail.com>
2021-03-06 00:08:18 +0300

    Edit role API

--- server/src/routes/groups.js
+++ server/src/routes/groups.js
@@ -6,6 +6,7 @@ const authorizedOnly = require('../service/authorizedOnly.js');
 const groupsCollection = require('../database/groups.js');
 const {v4: uuidv4} = require('uuid');
 const generateInviteCode = require('../service/inviteCode.js');
+const permissions = require('../service/permissions.js');
 
 /*
 
@@ -91,6 +92,81 @@ groups.post('/join', authorizedOnly, async (req, res) => {
             response.message = 'Вы успешно присоединились к группе';
         }
         
+    }
+
+    res.json(response);
+});
+
+groups.post('/edit-role', authorizedOnly, async (req, res) => {
+    let senderId = res.locals.userId;
+    let { groupId, userId, newRole } = req.body;
+    let response = {};
+
+    if ( !['member', 'editor', 'admin', 'owner'].includes(newRole) ) {
+        res.status(400);
+        response.ok = false;
+        response.message = 'Неверное название роли. Доступные роли: member, editor, admin, owner.';
+        return res.json(response);
+    } else if (senderId == userId) {
+        res.status(400);
+        response.ok = false;
+        response.message = 'Нельзя изменить права самому себе';
+        return res.json(response);
+    }
+
+    // #1 Поиск группы
+    let group = await groupsCollection.findGroup({id: groupId});
+    if (!group) {
+        res.status(400);
+        response.ok = false;
+        response.message = 'Неверный id группы';
+    } else {
+        // #2 Поиск отправителя и получателя в группе, определение их ролей
+        let senderRole = null;
+        let recipientRole = null;
+        for (let user of group.users) {
+            if (user.id == senderId) {
+                senderRole = user.role;
+            } else if (user.id == userId) {
+                recipientRole = user.role
+            }
+        }
+
+        if (!senderRole) {
+            res.status(400);
+            response.ok = false;
+            response.message = 'Вы не состоите в данной группе';
+        } else if (!recipientRole) {
+            res.status(400);
+            response.ok = false;
+            response.message = 'Запрашиваемый пользователь не состоит в группе';
+        } else if (recipientRole == newRole) {
+            res.status(400);
+            response.ok = false;
+            response.message = 'Пользователь уже имеет такие права';
+        } else {
+            // #3 Определение имеет ли отправитель право на данное действие
+            let permission = recipientRole + '-set-' + newRole;
+
+            if ( !permissions[senderRole].includes(permission) ) {
+                res.status(400);
+                response.ok = false;
+                response.message = 'У вас недостаточно прав для данного действия';
+            } else {
+                // Проверки пройдены, изменяем права пользователя
+                for (let user of group.users) {
+                    if (user.id == userId) {
+                        user.role = newRole;
+                    }
+                }
+
+                await groupsCollection.updateGroup({id: groupId}, {$set: {users: group.users}});
+
+                res.status(200);
+                response.ok = true;
+                response.message = 'Права пользователя успешно изменены';
+            }
+        }
     }
 
     res.json(response);
