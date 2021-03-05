5ce20cebb60802c8128a87c07d2a0a1f64610994
Volosyuk Daniil <danya.volosyuk@gmail.com>
2021-03-05 23:28:53 +0300

    Role permissions

---
+++ server/src/service/permissions.js
@@ -0,0 +1,16 @@
+// Роли и их права в группах
+
+const memberPermissions = ['commentPost', 'voteQuiz'];
+
+const editorPermissions = ['createPost', 'createQuiz'].concat(memberPermissions);
+
+const adminPermissions = ['kick-member', 'kick-editor', 'member-set-editor', 'editor-set-member', 'editGroup', 'updateInviteCode'].concat(editorPermissions);
+
+const ownerPermissions = ['kick-admin', 'member-set-admin', 'editor-set-admin', 'admin-set-editor', 'admin-set-member', 'transferOwnerRights'].concat(adminPermissions);
+
+module.exports = {
+    member: memberPermissions,
+    editor: editorPermissions,
+    admin: adminPermissions,
+    owner: ownerPermissions
+};
