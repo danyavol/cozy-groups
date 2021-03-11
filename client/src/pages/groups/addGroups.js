07e5251f2d2584eaf2839b79881b41ec605ae478
Roman <56370973+RomaTocks@users.noreply.github.com>
2021-03-11 11:12:58 +0300

    Error block and loadings added.

--- client/src/pages/groups/addGroups.js
+++ client/src/pages/groups/addGroups.js
@@ -7,27 +7,39 @@ class addGroups extends Component {
         super(props);
         this.state = {
             token:null,
+
+            loading : false,
+
             inviteCode : '',
-            groupName : ''
+            groupName : '',
+            
+            globalErrorTitle: '',
+            globalError: '',
+
         }
         this.joinGroup = this.joinGroup.bind(this);
         this.createGroup = this.createGroup.bind(this);
         this.handleGroupNameChange = this.handleGroupNameChange.bind(this);
         this.handleInviteCodeChange = this.handleInviteCodeChange.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.hideGlobalError = this.hideGlobalError.bind(this);
     }
     
     render() {
         return (
             <div>
-                <div className="ui very padded text container segment">
-                    <h2 className="ui header">Add Group page.</h2>
+                <div className={`ui negative message ${this.state.globalError ? '' : 'hidden'}`}>
+                        <i className="close icon" onClick={this.hideGlobalError}></i>
+                        <div className="header">{this.state.globalErrorTitle}</div>
+                        <p>{this.state.globalError}</p>
                 </div>
-                <div className="ui placeholder segment">
+                <div className="ui placeholder very padded container segment">
+                    
                     <div className="ui two column stackable center aligned grid">
                         <div className="ui vertical divider">или</div>
                             <div className="middle aligned row">
                                 <div className="column">
+                                    
                                     <div className="ui icon header"><i className="search icon"></i> Присоединиться </div>
                             <div className="ui search">
                                 <div className="ui icon input">
@@ -43,7 +55,10 @@ class addGroups extends Component {
                                 </div>
                                 <div className="results"></div>
                             </div>
-                            <div onClick={this.joinGroup} className="ui primary button">Присоединиться </div> 
+                            <div className="ui hidden divider"></div>
+                            <div className = "margin">
+                                <div onClick={this.joinGroup} className={`ui primary button ${this.state.loading ? 'loading disabled' : ''}`}>Присоединиться </div> 
+                            </div>
                         </div>
                         <div className="column">
                             <div className="ui icon header"><i className="world icon"></i> Создать группу </div>
@@ -61,51 +76,77 @@ class addGroups extends Component {
                                     </div>
                                     <div className="results"></div>
                                 </div>
-                                <div onClick={this.createGroup} className="ui primary button">Создать </div>
+                                <div className="ui hidden divider"></div>
+                                <div className = "margin">
+                                    <div onClick={this.createGroup} className={`ui primary button ${this.state.loading ? 'loading disabled' : ''}`}>Создать </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <button>Да</button>
             </div>
         );
     }
 
     createGroup(){
+        this.setState({ loading: true });
+        this.hideGlobalError();
         let data = {name : this.state.groupName}
         axios.post('http://localhost:3080/groups/create',data, {
             headers: {
                 'Authorization': this.props.token
             }})
             .then(response => {
+                this.setState({ loading: false });
                 if(response.data.ok) {
                     console.log('Успешно создана группа!')
                 }
             })
         .catch((err) => {
+            this.setState({ loading: false });
             let errorText;
             if (err.response) errorText = err.response.data.message;
             else errorText = 'Ошибка соединения с сервером';
-            console.log(errorText);
+            this.setState({
+                globalErrorTitle: 'Ошибка',
+                globalError: errorText,
+                loading: false
+            });
         })
     }
 
-joinGroup() {
-    let data = {inviteCode : this.state.inviteCode}
+
+    hideGlobalError() {
+        this.setState({
+            globalErrorTitle: '',
+            globalError: ''
+        });
+    }
+
+    joinGroup() {
+        this.setState({ loading: true });
+        this.hideGlobalError();
+        let data = {inviteCode : this.state.inviteCode}
         axios.post('http://localhost:3080/groups/join',data, {
             headers: {
                 'Authorization': this.props.token
             }})
             .then(response => {
+                this.setState({ loading: false });
                 if(response.data.ok) {
                     console.log('Вы присоединились к группе!')
                 }
             })
         .catch((err) => {
+            this.setState({ loading: false });
             let errorText;
             if (err.response) errorText = err.response.data.message;
             else errorText = 'Ошибка соединения с сервером';
-            console.log(errorText);
+            this.setState({
+                globalErrorTitle: 'Ошибка',
+                globalError: errorText,
+                loading: false
+            });
         })
 }
 
