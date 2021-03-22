0b464d21cd7f20bd2513ce9150589ed5b4ec37eb
danilsahuta <danil.saxuta@icloud.com>
2021-03-22 19:00:07 +0300

    Group link invite code card

--- client/src/pages/groups/group.js
+++ client/src/pages/groups/group.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tab } from 'semantic-ui-react';
 import { Dropdown } from "semantic-ui-react";
+import { CopyToClipboard } from "react-copy-to-clipboard/lib/Component";
 
 import './group.css';
 import axios from "axios";
@@ -24,19 +25,9 @@ class Group extends React.Component {
 
             <div>
                 <Loader loading={this.state.loading} />
-                {/*<div className={`ui ${this.state.loading ? 'active' : 'disabled'}  loader`}></div>*/}
-                {/*/!*<div className={`holder ${this.state.loading ? '' : 'hidden'}`}>*!/*/}
-                {/*/!*    <div className={`ui middle aligned grid`}>*!/*/}
-                {/*/!*        <div className="eight column wide">*!/*/}
-                {/*/!*            <div className={`ui active centered large text loader`}>Загрузка группы...</div>*!/*/}
-                {/*/!*        </div>*!/*/}
-                {/*/!*    </div>*!/*/}
-                {/*/!*</div>*!/*/}
-
                 <div className={this.state.loading ? 'hidden' : ''}>
                     <div className="header">
                         <div className="buttons">
-                            <LinkDropdown />
                             <SettingsDropdown />
                         </div>
                         <div>
@@ -142,16 +133,57 @@ function Tabs(props) {
                 {key: 'users', icon: 'users', content: 'Пользователи'},
             render: () => <Tab.Pane attached={false}><UsersMenu state={props.state} /></Tab.Pane>
         },
+        {
+            menuItem:
+                {key: 'invite', icon: 'linkify', content: 'Ссылка для приглашения'},
+            render: () => <InviteTab inviteCode={props.state.group.inviteCode} copied={props.state.copied} />
+        }
     ];
 
     const GroupTabs = () => (
         <Tab menu={{secondary: true, pointing: true}} panes={panes} />
     );
 
-    return (<GroupTabs />);
+    return (
+            <GroupTabs />
+        );
 }
 
 
+class InviteTab extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            copied: false
+        }
+    }
+    render() {
+        return (
+            <div className="inviteCard">
+                <div className="ui fluid centered card inviteCard">
+                    <div className="content">
+                        <a className="center aligned header">Код приглашения</a>
+                        <div className="center aligned description">
+                            <h1 id="inviteCode">
+                                {this.props.inviteCode}
+                                <CopyToClipboard text={this.props.inviteCode} onCopy={() => this.setState({copied: true})}>
+                                    <h2><i className={`copy ${this.state.copied ? '' : 'disabled'} icon`}></i></h2>
+                                </CopyToClipboard>
+                            </h1>
+
+                        </div>
+                    </div>
+                    <div className="center aligned extra content">
+                        <div className="large fluid ui button">Обновить</div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+
+
+}
 
 function UsersMenu(props) {
     if (props.state.group.length !== 0){
@@ -196,6 +228,7 @@ function UsersRows(props) {
     }
 }
 
+
 function SettingsDropdown() {
     return (
         <Dropdown
@@ -217,24 +250,8 @@ function SettingsDropdown() {
     )
 }
 
-function LinkDropdown() {
-    return (
-        <Dropdown
-            icon="settings"
-            floating
-            button
-            direction="left"
-            className="icon"
-        >
-            <Dropdown.Menu>
-                <Dropdown.Header  content="Тут инвайт код" />
-                <Dropdown.Divider />
-                <Dropdown.Item>Скопировать</Dropdown.Item>
-                <Dropdown.Item>Обновить</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
-    )
-}
+
+
 function Loader(props) {
     return (
         <div className={`holder ${props.loading ? '' : 'hidden'}`}>
