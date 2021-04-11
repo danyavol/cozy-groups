6611401cdf766709b6052053e1a436efb6a4bb63
danilsahuta <danil.saxuta@icloud.com>
2021-04-11 14:34:29 +0300

    Refactoring

---
+++ client/src/pages/groups/groupHooks.js
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react';
+import "./group.css"
+import axios from "axios";
+
+import SettingsDropdown from "../../components/settingDropdown/SettingDropdown";
+import Loader from "../../components/loader/Loader";
+import GroupsTabs from "../../components/groupsTabs/GroupsTabs";
+
+function GroupHooks(props) {
+    const [token, setToken] = useState("")
+    const [title, setTitle] = useState("");
+    const [group, setGroup] = useState({});
+    const [loading, setLoader] = useState(true);
+
+    useEffect(() => {
+        setTitle(group.name);
+    }, [group]);
+
+    useEffect(() => {
+        document.title = title;
+    });
+
+    useEffect(() => {
+        setLoader(true);
+        setToken(props.token);
+        axios.get('http://localhost:3080/groups/' + props.match.params.id, {
+            headers: {
+                'Authorization': props.token
+            }
+        })
+            .then(response => {
+                if (response.data.ok) {
+                    setGroup(response.data.group);
+                    setLoader(false);
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [props.token, props.match.params.id]);
+
+    return (
+        <div>
+            <Loader loading={loading} text="Загрузка группы..." />
+            <div className={loading ? 'hidden' : ''}>
+                <div className="header">
+                    <div className="buttons">
+                        <SettingsDropdown />
+                    </div>
+                    <div>
+                        <h1>{title}</h1>
+                    </div>
+                </div>
+                <div>
+                    <GroupsTabs token={token} group={group} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default GroupHooks;
