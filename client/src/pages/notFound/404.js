9135e66ffc40e2350e54dd856bd04acb5bcd593e
Roman <56370973+RomaTocks@users.noreply.github.com>
2021-05-15 00:45:20 +0300

    Cozy update for 404.

--- client/src/pages/notFound/404.js
+++ client/src/pages/notFound/404.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import "./404.css"
 class Error extends Component {
     componentDidMount() {
@@ -7,18 +7,20 @@ class Error extends Component {
     }
     getRandomInt(max) {
         return Math.floor(Math.random() * max);
-      }
+    }
     number = null;
-    phrases = ['ЁМАЁ','ЁЛКИ ПАЛКИ','Не туда ты заполз пацанчик...','Мне кажется тебе не сюда :(','В соответсвии с законодательством о нарушении границ вы будете арестованы через 3,2...','От лица Cozy Groups заявляю: ты не тут где здесь!']
+    phrases = ['ЁМАЁ', 'ЁЛКИ ПАЛКИ', 'Не туда ты заполз пацанчик...', 'Мне кажется тебе не сюда :(', 'В соответсвии с законодательством о нарушении границ вы будете арестованы через 3,2...', 'От лица Cozy Groups заявляю: ты не тут где здесь!']
     render() {
         return (
-           <div className="error-block">
-               <h1 className="error-block-text">404</h1>
-               <h4 className="phrase">{this.phrases[this.number]}</h4>
-               <a className="ui huge button" href="/"><i className="reply icon"></i>Вернуться на главную страницу</a>
-           </div>
+            <Fragment>
+                <div className="error-block">
+                    <img className="error-image" src="/images/cozy-error.png" alt="site-logo" />
+                    <h4 className="phrase">{this.phrases[this.number]}</h4>
+                    <a className="ui huge button" href="/"><i className="reply icon"></i>Вернуться на главную страницу</a>
+                </div>
+            </Fragment>
         );
     }
 }
-
+{/* <h1 className="error-block-text">404</h1> */ }
 export default Error;
