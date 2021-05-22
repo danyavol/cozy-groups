9ac0f5379fada58346a7f79187f28ffb708264f1
Volosyuk Daniil <danya.volosyuk@gmail.com>
2021-05-22 03:50:34 +0300

    Added quiz voting endpoint

--- server/src/service/dto.js
+++ server/src/service/dto.js
@@ -4,36 +4,37 @@ const usersCollection = require('../database/database.js')('users');
 
 // data  =>  single post or array
 exports.getPostDTO =  async function (data) {
+    const allUsers = await usersCollection.find(null, true);
 
     if (Array.isArray(data)) {
-        const allUsers = await usersCollection.find(null, true);
-
         data.forEach(post => {
             const user = allUsers.find(user => user.id === post.authorId);
-            post = handleSinglePost(post, user);
+            post = handleSinglePost(post, user, allUsers);
         });
     } 
     else {
-        const author = await usersCollection.find({id: data.authorId});
-        
-        data = handleSinglePost(data, author);
+        const author = allUsers.find(user => user.id === data.authorId);
+        data = handleSinglePost(data, author, allUsers);
     }
     
     return data;
 
 
 
-    function handleSinglePost(post, author={}) {
-        post.author = {
-            id: author.id,
-            login: author.login,
-            firstName: author.firstName,
-            lastName: author.lastName
-        };
+    function handleSinglePost(post, author={}, allUsers) {
+        post.author = getAuthor(author);
         post.totalComments = post.comments.length;
     
         const fieldsToDelete = ['_id', 'groupId', 'authorId', 'comments'];
         fieldsToDelete.forEach(field => delete post[field]);
+
+        if (post.type == 'quiz') {
+            post.votes.forEach(vote => {
+                const user = allUsers.find(user => user.id === vote.userId);
+                vote.user = getAuthor(user);
+                delete vote.userId;
+            })
+        }
 
         return post;
     }
@@ -63,12 +64,7 @@ exports.getCommentDTO =  async function (data) {
 
 
     function handleSingleComment(comment, author={}) {
-        comment.author = {
-            id: author.id,
-            login: author.login,
-            firstName: author.firstName,
-            lastName: author.lastName
-        };
+        comment.author = getAuthor(author);
     
         const fieldsToDelete = ['_id', 'groupId', 'postId', 'authorId'];
         fieldsToDelete.forEach(field => delete comment[field]);
@@ -76,4 +72,15 @@ exports.getCommentDTO =  async function (data) {
         return comment;
     }
 
+}
+
+
+
+function getAuthor(author) {
+    return {
+        id: author.id,
+        login: author.login,
+        firstName: author.firstName,
+        lastName: author.lastName
+    };
 }
